23cc618da6d16c90ca4c1a87cf380d641de054b3
mobin <mnlarijanian@gmail.com>
2021-06-12 10:20:19 +0430

    sprint 4

--- src/app/features/question/List.tsx
+++ src/app/features/question/List.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Badge, Button, Card, ListGroup, ListGroupItem, Spinner } from "react-bootstrap";
+import { Accordion, Badge, Button, Card, ListGroup, ListGroupItem, Spinner, Table } from "react-bootstrap";
 import useSWR from "swr";
 
 import { nodeFetcher } from "../../logic";
@@ -31,32 +31,65 @@ export default function QuestionsList({
                     <Card.Header>
                         <div className="d-flex justify-content-between align-items-center">
                             <Accordion.Toggle as={Button} variant="link" eventKey={String(i)}>
-                                <span className="text-muted">
-                                    {q.part} - {q.content}
+                                <span className="text-muted d-flex">
+                                    {q.part} - <div dangerouslySetInnerHTML={{ __html: q.content }} />
                                 </span>
                             </Accordion.Toggle>
+                            <div className="mr-auto" />
+                            <Button variant="danger">
+                                <i className="bi bi-trash" />
+                            </Button>
+                            <Button variant="warning" className="mx-3">
+                                <i className="bi bi-pencil" />
+                            </Button>
                             <Button onClick={() => handleAddSubQuestion(q._id as any)}>+</Button>
                         </div>
                     </Card.Header>
                     <Accordion.Collapse eventKey={String(i)}>
                         <Card.Body>
-                            {q?.subQuestions &&
-                                q?.subQuestions.map((sq) => (
-                                    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 2fr 1fr", gap: 10 }}>
-                                        <span>{sq.type}</span>
-                                        <span dangerouslySetInnerHTML={{ __html: sq.content }} />
-                                        <span>
-                                            <Badge variant="primary">{sq.options?.A}</Badge>{" "}
-                                            <Badge variant="primary">{sq.options?.B}</Badge>{" "}
-                                            <Badge variant="primary">{sq.options?.C}</Badge>{" "}
-                                            <Badge variant="primary">{sq.options?.D}</Badge>{" "}
-                                            <Badge variant="primary">{sq.options?.E}</Badge>{" "}
-                                        </span>
-                                        <span>
-                                            answer:<Badge variant="success">{sq?.answer?.join(" - ")}</Badge>
-                                        </span>
-                                    </div>
-                                ))}
+                            <Table responsive size="small">
+                                <thead>
+                                    <tr>
+                                        <th>Type</th>
+                                        <th>Content</th>
+                                        <th>Options</th>
+                                        <th>Answer</th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {q?.subQuestions &&
+                                        q?.subQuestions.map((sq) => (
+                                            <tr>
+                                                <td>{sq.type}</td>
+                                                <td dangerouslySetInnerHTML={{ __html: sq.content }} />
+                                                <td>
+                                                    <Badge variant="primary">{sq.options?.A}</Badge>{" "}
+                                                    <Badge variant="primary">{sq.options?.B}</Badge>{" "}
+                                                    <Badge variant="primary">{sq.options?.C}</Badge>{" "}
+                                                    <Badge variant="primary">{sq.options?.D}</Badge>{" "}
+                                                    <Badge variant="primary">{sq.options?.E}</Badge>{" "}
+                                                </td>
+                                                <td>
+                                                    {sq?.answer?.map((a: string) => (
+                                                        <Badge variant="success">{a}</Badge>
+                                                    ))}
+                                                </td>
+                                                <td>
+                                                    <Button variant="warning" className="mx-3">
+                                                        <i className="bi bi-pencil" />
+                                                    </Button>
+                                                </td>
+                                                <td>
+                                                    <Button variant="danger">
+                                                        <i className="bi bi-trash" />
+                                                    </Button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </Table>
                         </Card.Body>
                     </Accordion.Collapse>
                 </Card>
